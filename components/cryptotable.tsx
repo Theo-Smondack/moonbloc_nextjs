8fb68963865b223fa0d6626d42599f3df61e9531
TheoSmond <smondacktheo@gmail.com>
2022-11-18 10:27:26 +0100

    Update cryptotable UI

--- components/cryptotable.tsx
+++ components/cryptotable.tsx
@@ -7,9 +7,9 @@ import {Currency} from "../types/currency";
 import {CryptoData} from "../types/cryptoData";
 
 
-const fetcher = async (url: RequestInfo | URL)  => {
+const fetcher = async (url: RequestInfo | URL) => {
     const res = await fetch(url)
-    if(!res.ok){
+    if (!res.ok) {
         const error = new SwrErr('An error occurred while fetching the data.');
         // Attach extra info to the error object.
         error.info = await res.json()
@@ -22,56 +22,56 @@ const fetcher = async (url: RequestInfo | URL)  => {
 
 const Cryptotable: React.FC = () => {
     const value = useCurrencyContext();
-    const currencySelected: Currency|undefined = value.state.currency;
-    const _symbol : string | undefined = currencySelected?.symbol;
-    const {data,error} = useSWR<CryptoData[]>(`/api/cryptocurrency/list/${currencySelected?.value}`,fetcher)
+    const currencySelected: Currency | undefined = value.state.currency;
+    const _symbol: string | undefined = currencySelected?.symbol;
+    const {data, error} = useSWR<CryptoData[]>(`/api/cryptocurrency/list/${currencySelected?.value}`, fetcher)
     if (error) return <div className='container'> failed to load </div>
     if (!data) return <div className='container'> loading... </div>
-    return(
-            <div className='container' style={{overflow:"auto"}}>
-                <table className={styles.crypto_table}>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>1h %</th>
-                        <th>24h %</th>
-                        <th>7d %</th>
-                        <th>Market cap</th>
-                        <th>Volume(24h)</th>
-                        <th>Circulating Supply</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.map(({id,cmc_rank,name,symbol,quote,total_supply}) => (
-                        <tr key={id}>
-                            <td>{cmc_rank}</td>
-                            <td>
-                                <div className={styles.name_div}>
-                                    <Image
-                                        src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${id}.png`}
-                                        width={24}
-                                        height={24}
-                                        alt="Test"
-                                    />
-                                    <p>{name}</p>
-                                    <p>{symbol}</p>
+    return (
+        <div className='container' style={{overflow: "auto"}}>
+            <table className={styles.crypto_table}>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>1h %</th>
+                    <th>24h %</th>
+                    <th>7d %</th>
+                    <th>Market cap</th>
+                    <th>Volume(24h)</th>
+                    <th>Circulating Supply</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map(({id, cmc_rank, name, symbol, quote, total_supply}) => (
+                    <tr key={id}>
+                        <td>{cmc_rank}</td>
+                        <td>
+                            <div className={styles.name_div} onClick={() => console.log(`id: ${id} name: ${name}`)}>
+                                <Image
+                                    src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${id}.png`}
+                                    width={24}
+                                    height={24}
+                                    alt="Test"
+                                />
+                                <p>{name}</p>
+                                <p className={'txtGrey'} style={{fontWeight:300}}>{symbol}</p>
 
-                                </div>
-                            </td>
-                            <td>{quote[`${currencySelected?.value}`].price.toFixed(2)} {_symbol}</td>
-                            <td>{Math.round(quote[`${currencySelected?.value}`].percent_change_1h*100)/100}</td>
-                            <td>{Math.round(quote[`${currencySelected?.value}`].percent_change_24h*100)/100}</td>
-                            <td>{Math.round(quote[`${currencySelected?.value}`].percent_change_7d*100)/100}</td>
-                            <td>{quote[`${currencySelected?.value}`].market_cap.toFixed(2)} {_symbol}</td>
-                            <td>{quote[`${currencySelected?.value}`].volume_24h.toFixed(2)} {_symbol}</td>
-                            <td>{Math.round(total_supply)} {_symbol}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
+                            </div>
+                        </td>
+                        <td>{quote[`${currencySelected?.value}`].price.toFixed(2)} {_symbol}</td>
+                        <td>{Math.round(quote[`${currencySelected?.value}`].percent_change_1h * 100) / 100}</td>
+                        <td>{Math.round(quote[`${currencySelected?.value}`].percent_change_24h * 100) / 100}</td>
+                        <td>{Math.round(quote[`${currencySelected?.value}`].percent_change_7d * 100) / 100}</td>
+                        <td>{quote[`${currencySelected?.value}`].market_cap.toFixed(2)} {_symbol}</td>
+                        <td>{quote[`${currencySelected?.value}`].volume_24h.toFixed(2)} {_symbol}</td>
+                        <td>{Math.round(total_supply)} {_symbol}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
