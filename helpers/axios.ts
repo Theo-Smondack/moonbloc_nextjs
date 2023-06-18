e6cbeb18d50901270b55038357a6b0d375ee265d
TheoSmond <smondacktheo@gmail.com>
2023-06-18 14:41:33 +0200

    Add axios instances

---
+++ helpers/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const coinGeckoApi = axios.create({
+    baseURL: 'https://api.coingecko.com/api/v3',
+    timeout: 1000,
+});
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000/api',
+    timeout: 1000,
+});
+
+export {coinGeckoApi,api};
