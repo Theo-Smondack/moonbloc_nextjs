3fac27dda29d89de15dfde1e9d51c60d847163c6
TheoSmond <smondacktheo@gmail.com>
2023-11-04 18:35:59 +0100

    fix(models/services): Update transactions models and services

--- models/Transaction.ts
+++ models/Transaction.ts
@@ -1,47 +1,55 @@
-import { CryptoData } from '../types/cryptoData';
-import { Currency } from '../types/currency';
-import mongoose, { model, models, Schema } from 'mongoose';
+import { CryptoData } from '../types/cryptoData'
+import { Currency } from '../types/currency'
+import mongoose, { Schema } from 'mongoose'
 
 export interface TransactionInput {
-    type: 'buy' | 'sell';
-    from: CryptoData['id'] | Currency['value'];
-    to: CryptoData['id'] | Currency['value'];
-    quantity: number;
-    price: number;
-    date: Date;
-    fee: number;
-
+  type: 'buy' | 'sell'
+  from: CryptoData['id'] | Currency['value']
+  to: CryptoData['id'] | Currency['value']
+  quantity: number
+  price: number
+  date: Date
+  fee: number
 }
 
 export class TransactionClass implements TransactionInput {
-    public type: 'buy' | 'sell';
-    public from: CryptoData['id'] | Currency['value'];
-    public to: CryptoData['id'] | Currency['value'];
-    public quantity: number;
-    public price: number;
-    public date: Date;
-    public fee: number;
-    public walletID: mongoose.Types.ObjectId;
+  public type: 'buy' | 'sell'
+  public from: CryptoData['id'] | Currency['value']
+  public to: CryptoData['id'] | Currency['value']
+  public quantity: number
+  public price: number
+  public date: Date
+  public fee: number
+  public walletID: mongoose.Types.ObjectId
 
-
-    constructor(type: 'buy' | 'sell', from: CryptoData['id'] | Currency['value'], to: CryptoData['id'] | Currency['value'], quantity: number, price: number, date: Date, fee: number, walletID: mongoose.Types.ObjectId) {
-        this.type = type;
-        this.from = from;
-        this.to = to;
-        this.quantity = quantity;
-        this.price = price;
-        this.date = date;
-        this.fee = fee;
-        this.walletID = walletID;
-    }
+  constructor(
+    type: 'buy' | 'sell',
+    from: CryptoData['id'] | Currency['value'],
+    to: CryptoData['id'] | Currency['value'],
+    quantity: number,
+    price: number,
+    date: Date,
+    fee: number,
+    walletID: mongoose.Types.ObjectId
+  ) {
+    this.type = type
+    this.from = from
+    this.to = to
+    this.quantity = quantity
+    this.price = price
+    this.date = date
+    this.fee = fee
+    this.walletID = walletID
+  }
 }
 
 export interface TransactionDocument extends TransactionClass, Document {
-    createdAt: Date;
-    updatedAt: Date;
+  createdAt: Date
+  updatedAt: Date
 }
 
-const TransactionSchema: Schema = new Schema({
+const TransactionSchema: Schema = new Schema(
+  {
     type: { type: String, required: true },
     from: { type: String, required: true },
     to: { type: String, required: true },
@@ -49,10 +57,14 @@ const TransactionSchema: Schema = new Schema({
     price: { type: Number, required: true },
     date: { type: Date, required: true },
     fee: { type: Number, required: true },
-    walletID: { type: mongoose.Types.ObjectId, required: true },
-}, {
+    walletID: { type: mongoose.Types.ObjectId, ref: 'Wallet', required: true },
+  },
+  {
     timestamps: true,
+  }
+)
 
-});
+TransactionSchema.index({ walletID: 1, from: 1, to: 1, type: 1 })
 
-export default models.Transaction || model<TransactionDocument>('Transaction', TransactionSchema);
+export default mongoose.models.Transaction ||
+  mongoose.model<TransactionDocument>('Transaction', TransactionSchema)
--- services/transactions.ts
+++ services/transactions.ts
@@ -1,13 +1,32 @@
-import Transaction, { TransactionClass, TransactionDocument, TransactionInput } from '../models/Transaction';
-import mongoose, { FilterQuery, QueryOptions } from 'mongoose';
-import { WalletDocument } from '../models/Wallet';
+import Transaction, {
+  TransactionClass,
+  TransactionDocument,
+  TransactionInput,
+} from '../models/Transaction'
+import mongoose, { FilterQuery, QueryOptions } from 'mongoose'
 
-export async function createTransaction(input:TransactionInput,walletID:mongoose.Types.ObjectId): Promise<TransactionDocument> {
-    const transaction: TransactionClass = new TransactionClass(input.type, input.from, input.to, input.quantity, input.price, input.date, input.fee, walletID);
-    return Transaction.create(transaction);
+export async function createTransaction(
+  input: TransactionInput,
+  walletID: mongoose.Types.ObjectId
+): Promise<TransactionDocument> {
+  const transaction: TransactionClass = new TransactionClass(
+    input.type,
+    input.from,
+    input.to,
+    input.quantity,
+    input.price,
+    input.date,
+    input.fee,
+    walletID
+  )
+  return Transaction.create(transaction)
 }
 
-export async function findTransactions(query:FilterQuery<TransactionDocument>,options:QueryOptions<WalletDocument> = { lean:true },fields:string | null = null) {
-    if (!query.walletID) throw new Error('walletID is required');
-    return Transaction.find(query,fields,options);
+export async function findTransactions(
+  query: FilterQuery<TransactionDocument>,
+  options: QueryOptions<TransactionDocument> = { lean: true },
+  fields: string | null = null
+) {
+  if (!query.walletID) throw new Error('walletID is required')
+  return Transaction.find(query, fields, options)
 }
