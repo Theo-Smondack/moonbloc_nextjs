271e4e29b4b59d4a91abfec0704a0066a6dfd7a7
TheoSmond <smondacktheo@gmail.com>
2023-11-25 12:38:31 +0100

    fix(models/types): Refactor Wallet model and types

--- models/Wallet.ts
+++ models/Wallet.ts
@@ -1,32 +1,55 @@
+import mongoose, { model, models, Schema } from 'mongoose'
 
-import mongoose, { model, models, Schema } from 'mongoose';
+// Types imports
+import { AssetList } from '../types/wallet'
 
 export interface WalletInput {
-    walletTitle: string;
+  walletTitle: string
+  assets?: AssetList
 }
 
 export class WalletClass implements WalletInput {
-    public walletTitle: string;
-    public userID: mongoose.Types.ObjectId;
+  public walletTitle: string
+  public userID: mongoose.Types.ObjectId
+  public assets?: AssetList
 
-    constructor(walletTitle: string, userID: mongoose.Types.ObjectId) {
-        this.walletTitle = walletTitle;
-        this.userID = userID;
-    }
+  constructor(
+    walletTitle: string,
+    userID: mongoose.Types.ObjectId,
+    assets?: AssetList
+  ) {
+    this.walletTitle = walletTitle
+    this.userID = userID
+    this.assets = assets
+  }
 }
 
 export interface WalletDocument extends WalletClass, Document {
-    _id: mongoose.Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
+  _id: mongoose.Types.ObjectId
+  createdAt: Date
+  updatedAt: Date
 }
 
-const WalletSchema: Schema = new Schema({
-        walletTitle: { type: String, required: true },
-        userID: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-    }, {
-        timestamps: true,
-    }
-);
+const WalletSchema: Schema = new Schema(
+  {
+    walletTitle: { type: String, required: true },
+    userID: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    assets: [{ type: Object }],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: false,
+    },
+  }
+)
 
-export default models.Wallet || model<WalletDocument>('Wallet', WalletSchema);
+WalletSchema.methods.getAssets = function (): AssetList {
+  return this.assets
+}
+
+WalletSchema.methods.getWalletTitle = function (): WalletInput['walletTitle'] {
+  return this.walletTitle
+}
+
+export default models.Wallet || model<WalletDocument>('Wallet', WalletSchema)
--- types/wallet.d.ts
+++ types/wallet.d.ts
@@ -1,55 +1,46 @@
-import { WalletDocument } from '../models/Wallet';
-import mongoose from 'mongoose';
-import { CryptoData } from './cryptoData';
-
+// Models imports
+import { WalletDocument } from '../models/Wallet'
 
 export type Asset = {
-    symbol: string,
-    name: string,
-    id: string,
-    image: string
-    price: number;
-    quantity: number;
-    total: number;
+  symbol: string
+  name: string
+  id: string
+  image: string
+  price: number
+  quantity: number
+  total: number
 }
-
-export type AssetApiResponse = Omit<Asset, 'total' | 'quantity'>;
-export type AssetApiResponseList = AssetApiResponse[];
-
-export type AssetList = Asset[];
+export type CGAsset = {
+  id: string
+  symbol: string
+  current_price: number
+  image: string
+  name: string
+}
+export type AssetList = Asset[]
 
 export type WalletResponse = {
-    ok : boolean;
-    wallet:{
-        walletTitle: WalletDocument['walletTitle'];
-        walletID: WalletDocument['_id'] | string;
-        assets: AssetList;
-    }
-
+  ok: boolean
+  wallet: {
+    walletTitle: WalletDocument['walletTitle']
+    walletID: WalletDocument['_id'] | string
+    assets: AssetList
+  }
 }
-
-export type WalletTransactionFilter = {
-    walletID: mongoose.Types.ObjectId;
-    assets?: CryptoData['id'][] | CryptoData['id'];
-    date?: Date;
-}
-export type WalletTransactionFilterWithoutAssets = Omit<WalletTransactionFilter, 'assets'>;
-
-export type WalletsState = WalletDocument[];
+export type WalletsState = WalletDocument[]
 
 export type WalletsContextType = {
-    wallets:WalletsState;
-    setWallets: (state:WalletsState) => void;
-
+  wallets: WalletsState
+  setWallets: (state: WalletsState) => void
 }
 
 export type WalletModalState = {
-    show: boolean;
-    type: 'create' | 'edit';
-    walletID?: string;
+  show: boolean
+  type: 'create' | 'edit'
+  walletID?: string
 }
 
 export type WalletModalContextType = {
-    state: WalletModalState;
-    setState: (state: WalletModalState) => void;
+  state: WalletModalState
+  setState: (state: WalletModalState) => void
 }
