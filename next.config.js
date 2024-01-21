62641a99e78d9d2052b0047cbdfbb3a798b794f4
TheoSmond <smondacktheo@gmail.com>
2024-01-21 17:10:40 +0100

    Update : Upgrade to Next.Js 14 + install ts-node to execute TypeScript scripts

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images : {
-    domains:['assets.coingecko.com']
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.coingecko.com',
+        pathname: '**',
+      },
+    ],
   },
-  typescript:{
-    ignoreBuildErrors:false,
-  }
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig
