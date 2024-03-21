6be75299e4eb2125bd5440cb839cdf17eb9805ab
Théo Smondack <theo.smondack@onefirstgroup.com>
2024-03-21 19:26:29 +0100

    Some refactor

--- pages/portfolio/[id].tsx
+++ pages/portfolio/[id].tsx
@@ -1,85 +1,105 @@
-import React, { ReactElement, useEffect, useState } from 'react';
-import Layout from '../../components/layout/layout';
-import { NextPageWithLayout } from '../_app';
-import { useRouter } from 'next/router';
-import { Asset, AssetList, WalletResponse } from '../../types/wallet';
-import useSWR from 'swr';
-import { defaultDataFetcher } from '../../helpers/fetchers';
+import React, { ReactElement, useEffect, useState } from 'react'
+import Layout from '../../components/layout/layout'
+import { NextPageWithLayout } from '../_app'
+import { useRouter } from 'next/router'
+import { Asset, AssetList, WalletResponse } from '../../types/wallet'
+import useSWR from 'swr'
+import { defaultDataFetcher } from '../../helpers/fetchers'
 import styles from './portfoliopage.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import AssetSelectorModal, { searchValue } from '../../components/portfolio/portfolioPage/assetSelectorModal';
-import AssetsList from '../../components/portfolio/portfolioPage/assetsList';
-import { useCurrencyContext } from '../../context/currency';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import AssetSelectorModal, {
+  searchValue,
+} from '../../components/portfolio/portfolioPage/assetSelectorModal'
+import AssetsList from '../../components/portfolio/portfolioPage/assetsList'
+import { useCurrencyContext } from '../../context/currency'
 
 const PortfolioPage: NextPageWithLayout = () => {
-    const { query: { id } } = useRouter()
-    const [url, setUrl] = useState<string | null>(null)
-    const [showModal, setShowModal] = useState<boolean>(false)
-    const { state:{ currency } } = useCurrencyContext()
+  const {
+    query: { id },
+  } = useRouter()
+  const [url, setUrl] = useState<string | null>(null)
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const {
+    state: { currency },
+  } = useCurrencyContext()
 
-    const openModal = (show:boolean) => {
-        setShowModal(show)
+  const openModal = (show: boolean) => {
+    setShowModal(show)
+  }
+  const [portfolio, setPortfolio] = useState<WalletResponse['wallet']>({
+    walletTitle: '',
+    walletID: '',
+    assets: [],
+  })
+  useEffect(() => {
+    if (id) {
+      setUrl(
+        `/api/user/wallet/find/${id}?currency=${
+          currency ? currency.value : 'usd'
+        }`
+      )
     }
-    const [portfolio, setPortfolio] = useState<WalletResponse['wallet']>({
-        walletTitle: '',
-        walletID: '',
-        assets: [],
-    })
-    useEffect(() => {
-        if (id) {
-            setUrl(`/api/user/wallet/find/${id}?currency=${currency ? currency.value : 'usd'}`)
-        }
-    }, [id, currency])
+  }, [id, currency])
 
-    const { data, error } = useSWR<WalletResponse>(url, defaultDataFetcher)
+  const { data, error } = useSWR<WalletResponse>(url, defaultDataFetcher)
 
-
-    useEffect(() => {
-        if (data && !error) {
-            setPortfolio(data.wallet);
-        }
-    }, [data, error]);
-
-    const addAsset = (asset: searchValue) => {
-        const newAsset : Asset = {
-            id: asset.id, image: asset.image, name: asset.name, price: asset.price, quantity: 0, symbol: asset.symbol, total: 0,
-        }
-        const portfolioAssets = portfolio.assets
-        const newAssets:AssetList = [...portfolioAssets,newAsset]
-        setPortfolio({ ...portfolio, assets: newAssets })
+  useEffect(() => {
+    if (data && !error) {
+      setPortfolio(data.wallet)
     }
+  }, [data, error])
 
-    if (error) return <div className='container'> failed to load wallet</div>
-    if (!data) return <div>Loading</div>
-    const { walletTitle, walletID, assets } = portfolio;
-    return (
-        <div className={styles.container}>
-            {showModal ? <AssetSelectorModal showCallback={openModal} title={`Add crypto to ${walletTitle}`} addAsset={addAsset}/> : null}
-            <div className={styles.headContainer}>
-                <h1>{walletTitle}</h1>
-                <button className={`bgBlueButton ${styles.addTransactionButton}`}
-                        onClick={() => openModal(true)}>
-                    <FontAwesomeIcon icon={faCirclePlus} className={styles.icon}/>
-                    <p>Add asset</p>
-                </button>
-            </div>
-            {
-                assets.length ?
-                    <AssetsList Assets={assets}/>
-                    :
-                    <div className={styles.noAssetContainer}>There is no cryptocurrencies in this portfolio</div>
-            }
+  const addAsset = (asset: searchValue) => {
+    const newAsset: Asset = {
+      id: asset.id,
+      image: asset.image,
+      name: asset.name,
+      price: asset.price,
+      quantity: 0,
+      symbol: asset.symbol,
+      total: 0,
+    }
+    const portfolioAssets = portfolio.assets
+    const newAssets: AssetList = [...portfolioAssets, newAsset]
+    setPortfolio({ ...portfolio, assets: newAssets })
+  }
+
+  if (error) return <div className="container"> failed to load wallet</div>
+  if (!data) return <div>Loading</div>
+  const { walletTitle, assets } = portfolio
+  return (
+    <div className={styles.container}>
+      {showModal ? (
+        <AssetSelectorModal
+          showCallback={openModal}
+          title={`Add crypto to ${walletTitle}`}
+          addAsset={addAsset}
+        />
+      ) : null}
+      <div className={styles.headContainer}>
+        <h1>{walletTitle}</h1>
+        <button
+          className={`bgBlueButton ${styles.addTransactionButton}`}
+          onClick={() => openModal(true)}
+        >
+          <FontAwesomeIcon icon={faCirclePlus} className={styles.icon} />
+          <p>Add asset</p>
+        </button>
+      </div>
+      {assets.length ? (
+        <AssetsList Assets={assets} />
+      ) : (
+        <div className={styles.noAssetContainer}>
+          There is no cryptocurrencies in this portfolio
         </div>
-    );
+      )}
+    </div>
+  )
 }
 
 PortfolioPage.getLayout = function getLayout(page: ReactElement) {
-    return (
-        <Layout>
-            {page}
-        </Layout>
-    )
+  return <Layout>{page}</Layout>
 }
 
-export default PortfolioPage;
+export default PortfolioPage
